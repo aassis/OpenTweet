a94241902b5714d8410ca279fabf6d32d8cf9a91
André Assis <andre.assisn@gmail.com>
2024-07-16 19:30:13 -0700

    feat(bonus): added a custom selection state for displayed cells

--- OpenTweet/Feature/TweetList/TweetCell.swift
+++ OpenTweet/Feature/TweetList/TweetCell.swift
@@ -6,7 +6,9 @@ final class TweetCell: UITableViewCell {
     }
 
     struct Constants {
-        static let defaultPadding: CGFloat = 8.0
+        static let sidePaddings: CGFloat = 24.0
+        static let verticalPaddings: CGFloat = 8.0
+        static let insidePadding: CGFloat = 8.0
         static let avatarHeightWidth: CGFloat = 40.0
         static let avatarCornerRadius: CGFloat = 20.0
         static let fontSize: CGFloat = 14.0
@@ -57,6 +59,38 @@ final class TweetCell: UITableViewCell {
         labelDate.text = tweet.dateShortString
         labelContent.attributedText = tweet.tweetContent(fontSize: Constants.fontSize)
     }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        if isSelected && selected {
+            super.setSelected(false, animated: animated)
+            resetCell()
+        } else {
+            super.setSelected(selected, animated: animated)
+
+            if selected {
+                transformCell()
+            } else {
+                resetCell()
+            }
+        }
+    }
+
+    private func transformCell() {
+        UIView.animate(withDuration: 0.25) {
+            self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+            self.layer.shadowColor = UIColor.black.cgColor
+            self.layer.shadowOffset = CGSize(width: 0.0, height: 4.0)
+            self.layer.shadowOpacity = 0.3
+            self.layer.shadowRadius = 3.0
+        }
+    }
+
+    private func resetCell() {
+        UIView.animate(withDuration: 0.25) {
+            self.transform = .identity
+            self.layer.shadowOpacity = 0
+        }
+    }
 }
 
 extension TweetCell: ViewCode {
@@ -74,31 +108,34 @@ extension TweetCell: ViewCode {
     }
 
     func buildConstraints() {
-        let defaultPadding = Constants.defaultPadding
+        let sidePaddings = Constants.sidePaddings
+        let verticalPadding = Constants.verticalPaddings
+        let insidePadding = Constants.insidePadding
         let avatarHeightWidth = Constants.avatarHeightWidth
 
-        imageAvatar.topAnchor.constraint(equalTo: contentView.topAnchor, constant: defaultPadding).isActive = true
-        imageAvatar.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: defaultPadding).isActive = true
+        imageAvatar.topAnchor.constraint(equalTo: contentView.topAnchor, constant: verticalPadding).isActive = true
+        imageAvatar.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: sidePaddings).isActive = true
         imageAvatar.widthAnchor.constraint(equalToConstant: avatarHeightWidth).isActive = true
         imageAvatar.heightAnchor.constraint(equalToConstant: avatarHeightWidth).isActive = true
         imageAvatar.heightAnchor.constraint(equalToConstant: avatarHeightWidth).isActive = true
 
         labelAuthorName.topAnchor.constraint(equalTo: imageAvatar.topAnchor).isActive = true
-        labelAuthorName.leftAnchor.constraint(equalTo: imageAvatar.rightAnchor, constant: defaultPadding).isActive = true
+        labelAuthorName.leftAnchor.constraint(equalTo: imageAvatar.rightAnchor, constant: insidePadding).isActive = true
 
         labelDate.topAnchor.constraint(equalTo: labelAuthorName.topAnchor).isActive = true
-        labelDate.leftAnchor.constraint(equalTo: labelAuthorName.rightAnchor, constant: defaultPadding).isActive = true
-        labelDate.rightAnchor.constraint(lessThanOrEqualTo: contentView.rightAnchor, constant: -defaultPadding).isActive = true
+        labelDate.leftAnchor.constraint(equalTo: labelAuthorName.rightAnchor, constant: insidePadding).isActive = true
+        labelDate.rightAnchor.constraint(lessThanOrEqualTo: contentView.rightAnchor, constant: -sidePaddings).isActive = true
 
-        labelContent.topAnchor.constraint(equalTo: labelAuthorName.bottomAnchor, constant: defaultPadding).isActive = true
+        labelContent.topAnchor.constraint(equalTo: labelAuthorName.bottomAnchor, constant: insidePadding).isActive = true
         labelContent.leftAnchor.constraint(equalTo: labelAuthorName.leftAnchor).isActive = true
-        labelContent.rightAnchor.constraint(lessThanOrEqualTo: contentView.rightAnchor, constant: -defaultPadding).isActive = true
-        let constraint = labelContent.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -defaultPadding)
+        labelContent.rightAnchor.constraint(lessThanOrEqualTo: contentView.rightAnchor, constant: -sidePaddings).isActive = true
+        let constraint = labelContent.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -verticalPadding)
         constraint.priority = .defaultHigh
         constraint.isActive = true
     }
 
     func additionalConfiguration() {
         self.clipsToBounds = true
+        self.selectionStyle = .none
     }
 }
